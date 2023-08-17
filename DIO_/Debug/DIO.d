bc06c9e0bce703d7bc300564126df9c63c4017fa
Youssef Khaled <101673979+YoussefKhaledAhmed@users.noreply.github.com>
2023-08-18 01:44:47 +0300

    17/8/2023

--- DIO_/Debug/DIO.d
+++
@@ -1,7 +0,0 @@
-DIO.o DIO.o: ../DIO.c ../DIO.h ../common_macros.h ../standardTypes.h
-
-../DIO.h:
-
-../common_macros.h:
-
-../standardTypes.h:
--- DIO_/Debug/DIO_Exercise.d
+++
@@ -1,8 +0,0 @@
-DIO_Exercise.o DIO_Exercise.o: ../DIO_Exercise.c ../DIO.h \
-  ../common_macros.h ../standardTypes.h
-
-../DIO.h:
-
-../common_macros.h:
-
-../standardTypes.h:
